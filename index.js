881d0ab1446d7ca5fc3c9fa1f7dca8fa82f268cd
piotrmaz <maximus_decimus@o2.pl>
2020-04-29 02:23:30 +0200

    change contact page

--- index.js
+++ index.js
@@ -33,3 +33,12 @@ $(document).ready(function(){
     });
   }, false);
 })();
+
+
+$('#number').click(function() {
+    $(this).find('span').text( $(this).data('last') );
+});
+
+$('#email').click(function() {
+    $(this).find('span').text( $(this).data('last') );
+});
