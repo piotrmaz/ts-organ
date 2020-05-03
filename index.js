180ef5507afb6e4532443e6ba22626b869b822ad
piotrmaz <maximus_decimus@o2.pl>
2020-05-04 00:09:26 +0200

    menu fixed position

--- index.js
+++ index.js
@@ -42,3 +42,21 @@ $('#number').click(function() {
 $('#email').click(function() {
     $(this).find('span').text( $(this).data('last') );
 });
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navigation");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
